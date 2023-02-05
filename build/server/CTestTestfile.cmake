428ed0d5b317409ca9641e5c43f508f7a5ca9d45
QuietCelsy <95384971+QuietCelsy@users.noreply.github.com>
2023-02-05 21:54:51 +0000

    Add files via upload

---
+++ build/server/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/Celsy/almond_ws/src/server
+# Build directory: /home/Celsy/almond_ws/build/server
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
